3b3251d438d0e113be2004e5f21ca82c4c0e3aad
LauraAre <lauritadefuensa@gmail.com>
2024-12-13 13:52:52 +0100

    retoques

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./styles/main.scss"
+import gsap from "gsap";
+
+
+
+  gsap.from(".items-right img", { 
+    duration: 1.5, 
+    opacity: 0,      // La imagen comienza invisible
+    rotation: 180,   // Rota desde 180 grados (gira desde un ángulo)
+    ease: "back.out(1.7)" // Rebota suavemente al final
+  });
